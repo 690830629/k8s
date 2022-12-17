2079efc4361e1fec20f0904eb5287b81c5e5cbcf
wanglei07 <wanglei07@qiyi.com>
2022-12-17 18:37:12 +0800

    a

---
+++ src/main/java/com/wanglei/k8s/demo/K8SController.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2020 iQIYI.COM - All Rights Reserved.
+ * This file is part of Pizza project. Unauthorized copy
+ * of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ *
+ * Author(s): INQ Team <iig_ipd_inq@qiyi.com>
+ */
+package com.wanglei.k8s.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class K8SController {
+    @RequestMapping("/k8s")
+    public String k8s() {
+        return "hello K8s!";
+    }
+}
