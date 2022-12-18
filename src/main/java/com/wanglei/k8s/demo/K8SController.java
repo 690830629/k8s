91895724f63549deda4e0f844c5c5daecc8fb2ef
wanglei07 <wanglei07@qiyi.com>
2022-12-18 10:03:32 +0800

    a

--- src/main/java/com/wanglei/k8s/demo/K8SController.java
+++ src/main/java/com/wanglei/k8s/demo/K8SController.java
@@ -15,6 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class K8SController {
     @RequestMapping("/k8s")
     public String k8s() {
-        return "hello K8s1234!";
+        return "hello K8s1!";
     }
 }
